4874b3aa358c5e3b29ca56a02fce185416cdb7e6
Kyle Jordan <kjord021@fiu.edu>
2021-01-04 10:12:14 -0500

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        //called when going to /movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        //returns when we navigate to movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format($"pageIndex = {0} & sortBy={1}", pageIndex, sortBy))
+        }
+
+    }
+}
